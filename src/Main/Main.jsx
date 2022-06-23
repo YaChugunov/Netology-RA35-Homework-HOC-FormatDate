84043ace4c01d6fcfb33644787a25a18b99a0abb
YaChugunov <informed.looser@gmail.com>
2022-06-23 11:50:16 +0300

    2 files modified, 2 deleted

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,44 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import moment from 'moment';
+import React, {useState} from 'react';
 
-import '../style.css';
+function DateTime(props) {
+    return (
+        <p className="date">{props.date}</p>
+    )
+}
 
-import ItemList from '../ItemList/ItemList';
-//import ItemClass from '../ItemClass/ItemClass';
+function Video(props) {
+    return (
+        <div className="video">
+            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <DateTime date={props.date} />
+        </div>
+    )
+}
 
-export default function Main() {
-  const [records, setRecords] = useState([]);
-  const [form, setForm] = useState({ inputName: '', inputHoursDiff: '' });
+function VideoList(props) {
+    return props.list.map(item => <Video url={item.url} date={item.date} />);
+}
 
-  const handleChange = (name, value) => {
-    setForm((prevForm) => ({ ...prevForm, [name]: value }));
-    // console.log('>> ' + value);
-  };
-
-  const handleSubmit = () => {
-    // Текущая дата
-    var date = new Date();
-    // Час локальный
-    var hoursLocal = date.getHours();
-    // Час в GMT 0
-    var hoursGMT0 = date.getUTCHours();
-
-    setRecords((prevRecords) => [
-      ...prevRecords,
-      new ItemClass(form.inputName, hoursGMT0, Number(form.inputHoursDiff)),
+export default function App() {
+    const [list, setList] = useState([
+        {
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2017-07-31 13:24:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-03-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-02-03 23:16:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-01-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-01-01 16:17:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2017-12-02 05:24:00'
+        },
     ]);
 
-    setForm({ inputName: '', inputHoursDiff: '' });
-  };
-
-  const handleRemove = (id) => {
-    setRecords((prevRecords) => prevRecords.filter((obj) => obj.id !== id));
-  };
-
-  return (
-    <>
-      <div className="baseTimeGMT"></div>
-      <ItemList isChanged={false} records={records} onRemove={handleRemove} />
-    </>
-  );
+    return (
+        <VideoList list={list} />
+    );
 }
